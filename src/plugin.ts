d5afdda3e3153eb862e7fc1e11bdedc13d9a5686
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-16 09:45:37 -0500

    fix require.resolve usage

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,37 +1,40 @@
 import * as babel from "@babel/core"
+import { createRequire } from "module"
 import type { TransformResult } from "vite"
+
+const require = createRequire(import.meta.url)
 
 const sourceRegex = /\.(j|t)sx?$/
 const tsxRegex = /\.(j|t)sx$/ // all files are being interpreted as TS, so we'll treat JSX as TSX
 
 export default function macrosPlugin() {
-	return {
-		name: "babel-macros",
-		enforce: "pre",
-		async transform(source: string, filename: string) {
-			if (filename.includes("node_modules")) {
-				return undefined
-			}
+  return {
+    name: "babel-macros",
+    enforce: "pre",
+    async transform(source: string, filename: string) {
+      if (filename.includes("node_modules")) {
+        return undefined
+      }
 
-			if (!sourceRegex.test(filename)) {
-				return undefined
-			}
+      if (!sourceRegex.test(filename)) {
+        return undefined
+      }
 
-			const result = await babel.transformAsync(source, {
-				filename,
-				plugins: [
-					require.resolve("@babel/plugin-syntax-jsx"),
-					[
-						require.resolve("@babel/plugin-syntax-typescript"),
-						{ isTSX: tsxRegex.test(filename) },
-					],
-					require.resolve("babel-plugin-macros"),
-				],
-				babelrc: false,
-				configFile: false,
-				sourceMaps: true,
-			})
-			return result as TransformResult | null
-		},
-	} as const
+      const result = await babel.transformAsync(source, {
+        filename,
+        plugins: [
+          require.resolve("@babel/plugin-syntax-jsx"),
+          [
+            require.resolve("@babel/plugin-syntax-typescript"),
+            { isTSX: tsxRegex.test(filename) },
+          ],
+          require.resolve("babel-plugin-macros"),
+        ],
+        babelrc: false,
+        configFile: false,
+        sourceMaps: true,
+      })
+      return result as TransformResult | null
+    },
+  } as const
 }
