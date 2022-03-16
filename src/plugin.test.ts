2b80c635c29668d22e22851e29b52fa0020e7258
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-16 09:32:10 -0500

    modernize project

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -1,41 +1,42 @@
 import { join } from "path"
-import type { OutputChunk } from "rollup"
+import type { OutputChunk, RollupOutput } from "rollup"
 import { build, Plugin } from "vite"
+import { expect, test } from "vitest"
 import macrosPlugin from "./plugin"
 
 test("macros", async () => {
-	const code = `
+  const code = `
 	import preval from 'preval.macro'
 	const result = preval\`module.exports = 1 + 2\`
 	`
-	const result = await macrosPlugin().transform(code, "file.js")
-	expect(result?.code).toMatchInlineSnapshot(`"const result = 3;"`)
+  const result = await macrosPlugin().transform(code, "file.js")
+  expect(result?.code).toMatchInlineSnapshot(`"const result = 3;"`)
 })
 
 test("typechecks as a vite plugin", () => {
-	const plugin: Plugin = macrosPlugin()
+  const plugin: Plugin = macrosPlugin()
 })
 
 test(".jsx files shouldn't throw", async () => {
-	const result = await macrosPlugin().transform(`<div/>`, "file.jsx")
-	expect(result).not.toBeNull()
+  const result = await macrosPlugin().transform(`<div/>`, "file.jsx")
+  expect(result).not.toBeNull()
 })
 
 test("vite integration", async () => {
-	let result = await build({
-		root: join(__dirname, "../test/fixture"),
-		plugins: [macrosPlugin()],
-		logLevel: "silent",
-		build: {
-			minify: false,
-			write: false,
-		},
-	})
-	result = [result].flat()[0]
+  let result = await build({
+    root: join(__dirname, "../test/fixture"),
+    plugins: [macrosPlugin()],
+    logLevel: "silent",
+    build: {
+      minify: false,
+      write: false,
+    },
+  })
+  result = [result].flat()[0] as RollupOutput
 
-	const code = result.output.find(
-		(item): item is OutputChunk => item.type === "chunk",
-	)?.code
+  const code = result.output.find(
+    (item): item is OutputChunk => item.type === "chunk",
+  )?.code
 
-	expect(code).toContain(`alert("it's a secret\\n")`)
+  expect(code).toContain(`alert("it's a secret\\n")`)
 }, 10000)
